80bcd3c864980b4bc27babbcfd35d10562e88c8f
jennyhavens <jennifer.havens91@gmail.com>
2024-09-19 16:47:44 -0500

    Fix spelling and messages for clarity
    Messages explain rules and more clearly give responses to user input. Spelling errors were corrected.

--- javascript.js
+++ javascript.js
@@ -14,7 +14,7 @@ function getComputerChoice() {
 function getHumanChoice() {
     let response = prompt("Rock, paper or scissors?");
         if (response === null) {
-            alert ("Bye!");
+            alert ("Bye! Close the window or click okay to continue the game.");
         } else if (response.toLowerCase() === "rock") {
             return ("rock");
         } else if (response.toLowerCase() === "paper") {
@@ -22,7 +22,7 @@ function getHumanChoice() {
         } else if (response.toLowerCase() === "scissors") {
             return ("scissors");
         } else {
-            alert ("Incorrect response, try again!");
+            alert ("Incorrect response, try again in the next round!");
         }
 }
    
@@ -33,22 +33,26 @@ function playGame() {
     let computerScore = 0;
     let round = 0;
 
+    //Rules
+    alert ("Welcome to a game of rock, paper, scissors!");
+    alert ("You have 5 rounds to beat the computer!\n\n* rock beats scissors\n* scissors beat paper\n* and paper beats rock\n\nGood Luck!")
+
     //Function to a play one round
     function playRound(humanChoice,computerChoice) {
         if (humanChoice === "rock" && computerChoice === "scissors" 
             || humanChoice === "scissors" && computerChoice === "paper" 
             || humanChoice === "paper" && computerChoice === "rock") {
             humanScore++;
-            alert (`${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)} beats ${computerChoice}! Your point!\n Your Score = ${humanScore} \n Computer Score = ${computerScore}`);
+            alert (`${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)} beats ${computerChoice}! Your point!\n\nYour Score = ${humanScore} \nComputer Score = ${computerScore}`);
         } else if (humanChoice === "scissors" && computerChoice === "rock" 
             || humanChoice === "paper" && computerChoice === "scissors" 
             || humanChoice === "rock" && computerChoice === "paper") {
             computerScore++;
-            alert (`${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)} beats ${humanChoice}! Computer\'s point!\n Your Score = ${humanScore} \n Computer Score = ${computerScore}`);
+            alert (`${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)} beats ${humanChoice}! Computer\'s point!\n\nYour Score = ${humanScore} \nComputer Score = ${computerScore}`);
         } else if (humanChoice === "scissors" && computerChoice === "scissors" 
             || humanChoice === "paper" && computerChoice === "paper" 
             || humanChoice === "rock" && computerChoice === "rock") {
-            alert (`Tie! You both chose ${humanChoice}! No points rewarded.\n Your Score = ${humanScore} \n Computer Score = ${computerScore}`);
+            alert (`Tie! You both chose ${humanChoice}! No points rewarded.\n\nYour Score = ${humanScore} \nComputer Score = ${computerScore}`);
         } else {
             alert (`Your Score = ${humanScore} \n Computer Score = ${computerScore}`);
         }
@@ -78,15 +82,13 @@ function playGame() {
 
     function declareWinner() {
         if (humanScore > computerScore) {
-            alert (`The winner is ... YOU!! Congragulations!\n Your Score = ${humanScore} \n Computer Score = ${computerScore}`);
+            alert (`The winner is ... YOU!! Congratulations!\n\nYour Score = ${humanScore} \nComputer Score = ${computerScore}`);
         } else if (humanScore < computerScore) {
-            alert (`The winner is ... The Computer!\n Your Score = ${humanScore} \n Computer Score = ${computerScore}`);
+            alert (`The winner is ... The Computer!\n\nYour Score = ${humanScore} \nComputer Score = ${computerScore}`);
         } else {
-            alert (`No winner ... Its a tie!\n Your Score = ${humanScore} \n Computer Score = ${computerScore}`);
+            alert (`No winner ... Its a tie!\n\nYour Score = ${humanScore} \nComputer Score = ${computerScore}`);
         }
     }
-    //for (let i = 0; i < 5; i++) {
-    //}
 }
 
 playGame();
