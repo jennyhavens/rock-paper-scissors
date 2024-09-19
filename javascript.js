db81cd8487cbaca48691b864735cd80a27f2a9f9
jennyhavens <jennifer.havens91@gmail.com>
2024-09-18 19:07:04 -0500

    Add function for playing one round.
    When the user inputs rock, paper or scissors the computer also responds and points are given to the winner of the round.
    The user is told who won points and can see the score.

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
+//Gets the computer's choice
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3);
-    
         if (randomNumber === 0) {
             return("rock");
         } else if (randomNumber === 1) {
@@ -10,12 +10,11 @@ function getComputerChoice() {
         }
 }
 
-console.log(getComputerChoice());
-
+//Gets the player's choice
 function getHumanChoice() {
     let response = prompt("Rock, paper or scissors?");
         if (response === null) {
-            return ("Bye!");
+            alert ("Bye!");
         } else if (response.toLowerCase() === "rock") {
             return ("rock");
         } else if (response.toLowerCase() === "paper") {
@@ -23,33 +22,37 @@ function getHumanChoice() {
         } else if (response.toLowerCase() === "scissors") {
             return ("scissors");
         } else {
-            return ("Incorrect response, refresh the page and try again!");
+            alert ("Incorrect response, try again!");
         }
-}    
-console.log(getHumanChoice());
-    
-    
-//        if (response === null) {
-//           return("Bye!");
-//        } else if (response === "") {
-//            return ("No response, refresh the page and try again!");
-//        } else if (response ) {
-//            return(response.toLowerCase());
-//        }
-//}
+}
+   
+ //Variables to keep the score
+let humanScore = 0;
+let computerScore = 0;
+let round = 0;
 
-//console.log(getHumanChoice());
+    //Function to a play one round
+    function playRound(humanChoice,computerChoice) {
+        if (humanChoice === "rock" && computerChoice === "scissors" 
+            || humanChoice === "scissors" && computerChoice === "paper" 
+            || humanChoice === "paper" && computerChoice === "rock") {
+            humanScore++;
+            alert (`${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)} beats ${computerChoice}! Your point!\n Human Score = ${humanScore} \n Computer Score = ${computerScore}`);
+        } else if (humanChoice === "scissors" && computerChoice === "rock" 
+            || humanChoice === "paper" && computerChoice === "scissors" 
+            || humanChoice === "rock" && computerChoice === "paper") {
+            computerScore++;
+            alert (`${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)} beats ${humanChoice}! Computer\'s point!\n Human Score = ${humanScore} \n Computer Score = ${computerScore}`);
+        } else if (humanChoice === "scissors" && computerChoice === "scissors" 
+            || humanChoice === "paper" && computerChoice === "paper" 
+            || humanChoice === "rock" && computerChoice === "rock") {
+            alert (`Tie! You both chose ${humanChoice}! No points rewarded.\n Human Score = ${humanScore} \n Computer Score = ${computerScore}`);
+        }
 
-//let humanScore = 0;
-//let computerScore = 0;
+        console.log(computerChoice);
+        console.log(humanChoice);
+    }
 
-//function playRound(humanChoice,computerChoice) {
-//    if (humanChoice === "rock" && computerChoice === "scissors") {
-
-//    }
-//}
-
-//const humanSelection = getHumanChoice();
-//const computerSelection = getComputerChoice();
-
-//playRound(humanSelection, computerSelection);
+    let humanSelection = getHumanChoice();
+    let computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
